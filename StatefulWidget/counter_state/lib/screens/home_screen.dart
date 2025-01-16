41572c6924b5b0a0f8a15c13103e1d2fd73d4433
Jose-01010000 <josepuello112000@gmail.com>
2025-01-15 21:47:29 -0500

    Reemplazar la pantalla de inicio con una nueva implementación que incluye navegación a la pantalla de configuración y funciones para modificar el contador.

---
+++ StatefulWidget/counter_state/lib/screens/home_screen.dart
@@ -0,0 +1,104 @@
+import 'package:counter_state/screens/config_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  void addCustomValue(int value) {
+    setState(() {
+      counter = value;
+    });
+  }
+
+  void resetValue() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Counter State"),
+        leading: IconButton(
+          onPressed: () {
+            setState(() {
+              counter = 0;
+            });
+          },
+          icon: const Icon(Icons.refresh_rounded),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ConfigScreen(
+                    addCustomValue: addCustomValue,
+                    resetValue: resetValue,
+                    counter: counter,
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "contador",
+              style: TextStyle(fontSize: 40),
+            ),
+            Text(
+              "$counter",
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 90),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+            heroTag: "increment",
+            tooltip: "Incrementar contador",
+            child: const Icon(Icons.plus_one_rounded),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter--;
+              });
+            },
+            heroTag: "decrement",
+            tooltip: "Decrementar contador",
+            child: const Icon(Icons.exposure_minus_1_rounded),
+          ),
+        ],
+      ),
+    );
+  }
+}
