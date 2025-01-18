d3c430efb0f50be45cd9d5093eddbe538a49bcf3
Jose-01010000 <josepuello112000@gmail.com>
2025-01-18 18:51:14 -0500

    Actualizar el modelo de datos de imágenes: añadir nuevos campos como 'likes', 'author', 'source', 'description', 'date', 'location' y 'camera'; y modificar la función de conversión a/from JSON.

--- StatefulWidget/image_gallery/lib/models/images.dart
+++ StatefulWidget/image_gallery/lib/models/images.dart
@@ -1,19 +1,35 @@
 class ImageModel {
   final String imageUrl;
   final String name;
+  final int likes;
+  final String author;
+  final String source;
+  final String description;
+  final DateTime date;
+  final String location;
+  final String camera;
 
   ImageModel({
     required this.imageUrl,
     required this.name,
+    required this.likes,
+    required this.author,
+    required this.source,
+    required this.description,
+    required this.date,
+    required this.location,
+    required this.camera,
   });
 
   factory ImageModel.fromJson(Map<String, dynamic> json) => ImageModel(
         imageUrl: json["imageUrl"],
         name: json["name"],
+        likes: json["likes"],
+        author: json["author"],
+        source: json["source"],
+        description: json["description"],
+        date: DateTime.parse(json["date"]),
+        location: json["location"],
+        camera: json["camera"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "imageUrl": imageUrl,
-        "name": name,
-      };
 }
