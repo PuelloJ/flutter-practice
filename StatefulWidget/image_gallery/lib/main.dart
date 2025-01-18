11642e910bdf6297a3e89dcb852ff0255da17cba
Jose-01010000 <josepuello112000@gmail.com>
2025-01-18 18:50:53 -0500

    Añadir soporte para temas oscuros y claros en la aplicación, incluyendo la implementación de AppTheme y la opción de cambiar el tema en HomeGalleryScreen.

--- StatefulWidget/image_gallery/lib/main.dart
+++ StatefulWidget/image_gallery/lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:image_gallery/config/app_theme.dart';
 import 'package:image_gallery/screens/home_gallery_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool isDarkMode = true;
+
+  void _changeTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      darkTheme: ThemeData.dark(),
+    return MaterialApp(
+      // theme: AppTheme().getLightTheme(),
+      // darkTheme: AppTheme().getDarkTheme(),
+      // themeMode: ThemeMode.system,
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
       debugShowCheckedModeBanner: false,
-      home: const HomeGalleryScreen(),
+      home: HomeGalleryScreen(
+        changeTheme: _changeTheme,
+        darkMode: isDarkMode,
+      ),
     );
   }
 }
--- StatefulWidget/image_gallery/lib/screens/home_gallery_screen.dart
+++ StatefulWidget/image_gallery/lib/screens/home_gallery_screen.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:image_gallery/config/constants.dart';
 import 'package:image_gallery/config/utils.dart';
 import 'package:image_gallery/models/images.dart';
+import 'package:image_gallery/widgets/carousel_section.dart';
+import 'package:image_gallery/widgets/image_grid.dart';
 
 class HomeGalleryScreen extends StatefulWidget {
-  const HomeGalleryScreen({super.key});
+  const HomeGalleryScreen({
+    super.key,
+    required this.changeTheme,
+    required this.darkMode,
+  });
+
+  final VoidCallback changeTheme;
+  final bool darkMode;
 
   @override
   State<HomeGalleryScreen> createState() => _HomeGalleryScreenState();
 }
 
 class _HomeGalleryScreenState extends State<HomeGalleryScreen> {
-  CarouselController carouselController = CarouselController(initialItem: 1);
   List<ImageModel> images = [];
   bool isLoading = true;
 
@@ -30,23 +37,21 @@ class _HomeGalleryScreenState extends State<HomeGalleryScreen> {
   }
 
   @override
-  void dispose() {
-    carouselController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.sizeOf(context).height;
-
     return Scaffold(
       appBar: AppBar(
-        titleTextStyle: const TextStyle(fontSize: 25),
-        title: const Text("Galeria de imagenes"),
+        title: const Text(
+          "Galeria de imagenes",
+          style: TextStyle(fontSize: 25),
+        ),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.dark_mode_rounded),
+            onPressed: widget.changeTheme,
+            icon: Icon(
+              widget.darkMode
+                  ? Icons.light_mode_outlined
+                  : Icons.dark_mode_outlined,
+            ),
           )
         ],
       ),
@@ -59,123 +64,30 @@ class _HomeGalleryScreenState extends State<HomeGalleryScreen> {
           : Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ConstrainedBox(
-                  constraints: BoxConstraints(maxHeight: height / 3),
-                  child: CarouselView(
-                    backgroundColor: Colors.blueAccent,
-                    controller: carouselController,
-                    itemSnapping: true,
-
-                    // flexWeights: const <int>[1, 7, 1],
-                    itemExtent: height * 0.3,
-                    children: images.map((image) {
-                      return HeroLayoutCard(imageInfo: image);
-                    }).toList(),
-                  ),
+                CarouselSection(
+                  images: images,
                 ),
                 Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2),
-                    itemCount: images.length,
-                    itemBuilder: (context, index) {
-                      final ImageModel image = images[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GridTile(
-                          header: const Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(Icons.info_outline_rounded),
-                          ),
-                          footer: Text(
-                            image.name,
-                            textAlign: TextAlign.center,
-                          ),
-                          child: Container(
-                            margin: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: Colors.blueAccent,
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Image.asset(image.imageUrl),
-                          ),
-                        ),
-                      );
-                    },
+                  child: ImageGridSection(
+                    images: images,
                   ),
                 )
               ],
             ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: const Row(
-          children: [
-            Text('Agregar imagen'),
-            SizedBox(
-              width: 10,
-            ),
-            Icon(Icons.add_a_photo_rounded),
-          ],
-        ),
-      ),
+      floatingActionButton: const _FloatingButton(),
     );
   }
 }
 
-class HeroLayoutCard extends StatelessWidget {
-  const HeroLayoutCard({
-    super.key,
-    required this.imageInfo,
-  });
-
-  final ImageModel imageInfo;
+class _FloatingButton extends StatelessWidget {
+  const _FloatingButton();
 
   @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.sizeOf(context).width;
-    return Stack(
-      alignment: AlignmentDirectional.bottomStart,
-      children: <Widget>[
-        ClipRect(
-          child: OverflowBox(
-            maxWidth: width,
-            minWidth: width,
-            child: Image(
-              fit: BoxFit.cover,
-              image: AssetImage(imageInfo.imageUrl),
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                imageInfo.name,
-                overflow: TextOverflow.clip,
-                softWrap: false,
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge
-                    ?.copyWith(color: Colors.white),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Subtitulo",
-                overflow: TextOverflow.clip,
-                softWrap: false,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(color: Colors.white),
-              )
-            ],
-          ),
-        ),
-      ],
+    return FloatingActionButton.extended(
+      onPressed: () {},
+      icon: const Icon(Icons.add_a_photo_outlined),
+      label: const Text('Agregar imagen'),
     );
   }
 }
