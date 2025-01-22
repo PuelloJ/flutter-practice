a0288fd3d439851e963079fb89e15d93b4bd0917
Jose-01010000 <josepuello112000@gmail.com>
2025-01-21 21:09:14 -0500

    Actualizar constantes de datos de imágenes; refactorizar ImageModel para permitir campos opcionales y agregar soporte para archivos de imagen

--- StatefulWidget/image_gallery/lib/models/images.dart
+++ StatefulWidget/image_gallery/lib/models/images.dart
@@ -1,24 +1,32 @@
+import 'dart:io';
+
+enum ImageType { file, asset }
+
 class ImageModel {
-  final String imageUrl;
+  final String? imageUrl;
   final String name;
   final bool favorite;
   final String author;
-  final String source;
+  final String? source;
   final String description;
-  final DateTime date;
-  final String location;
-  final String camera;
+  final DateTime? date;
+  final String? location;
+  final String? camera;
+  final ImageType imageType;
+  final File? imageFile;
 
   ImageModel({
-    required this.imageUrl,
+    this.imageUrl,
     required this.name,
     required this.favorite,
     required this.author,
     required this.source,
     required this.description,
     required this.date,
-    required this.location,
-    required this.camera,
+    required this.imageType,
+    this.location,
+    this.camera,
+    this.imageFile,
   });
 
   factory ImageModel.fromJson(Map<String, dynamic> json) => ImageModel(
@@ -31,6 +39,7 @@ class ImageModel {
         date: DateTime.parse(json["date"]),
         location: json["location"],
         camera: json["camera"],
+        imageType: ImageType.asset,
       );
 
   set favorite(bool value) {
