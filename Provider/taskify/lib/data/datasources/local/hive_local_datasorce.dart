1c77b28f6670321b3eef0c1f08fc67ceb6dc021f
PuelloJ <josepuello112000@gmail.com>
2025-02-14 21:48:07 -0500

    Agregar fuente de datos local utilizando Hive para gestionar categorías y tareas

---
+++ Provider/taskify/lib/data/datasources/local/hive_local_datasorce.dart
@@ -0,0 +1,54 @@
+import 'package:hive/hive.dart';
+import 'package:taskify/data/models/category_model.dart';
+import 'package:taskify/data/models/task_model.dart';
+
+class HiveLocalDatasorce {
+  late Box<CategoryModel> categoryBox;
+  late Box<TaskModel> taskBox;
+
+  Future<void> init() async {
+    Hive.registerAdapter(CategoryModelAdapter());
+    Hive.registerAdapter(TaskModelAdapter());
+
+    categoryBox = await Hive.openBox<CategoryModel>('categories');
+    taskBox = await Hive.openBox<TaskModel>('tasks');
+  }
+
+  /// Categories
+
+  Future<void> addCategory(CategoryModel category) async {
+    await categoryBox.put(category.id, category);
+  }
+
+  Future<List<CategoryModel>> getCategories() async {
+    return categoryBox.values.toList();
+  }
+
+  Future<void> deleteCategory(String categoryId) async {
+    await categoryBox.delete(categoryId);
+    final tasks = await getTasksByCategory(categoryId);
+    for (var task in tasks) {
+      await deleteTask(task.id);
+    }
+  }
+
+  /// Tasks
+
+  Future<List<TaskModel>> getTasksByCategory(String categoryId) async {
+    return taskBox.values
+        .where((task) => task.categoryId == categoryId)
+        .toList();
+  }
+
+  Future<void> addTask(TaskModel task) async {
+    await taskBox.put(task.id, task);
+  }
+
+  Future<void> updateTask(TaskModel task) async {
+    await taskBox.put(task.id, task);
+  }
+
+  Future<void> deleteTask(String taskId) async {
+    await taskBox.delete(taskId);
+  }
+}
